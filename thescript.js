68fcb2895c15455855cfd25370f807bb5e9c6ac4
Ghezali Karim <75776752+ghezalikarim@users.noreply.github.com>
2021-01-26 22:09:40 +0100

    Add files via upload

---
+++ thescript.js
@@ -0,0 +1,39 @@
+let view_STABILA = function () {
+    document.getElementById("METABO_details").classList.remove("view_details");
+    document.getElementById("STIHL_details").classList.remove("view_details");
+    document.getElementById("HADEF_details").classList.remove("view_details");
+    document.getElementById("STABILA_details").classList.add("view_details");
+    document
+      .getElementById("STABILA_details")
+      .scrollIntoView({ behavior: "smooth" });
+  };
+  
+  let view_METABO = function () {
+    document.getElementById("STABILA_details").classList.remove("view_details");
+    document.getElementById("STIHL_details").classList.remove("view_details");
+    document.getElementById("HADEF_details").classList.remove("view_details");
+    document.getElementById("METABO_details").classList.add("view_details");
+    document
+      .getElementById("METABO_details")
+      .scrollIntoView({ behavior: "smooth" });
+  };
+  
+  let view_STIHL = function () {
+    document.getElementById("STABILA_details").classList.remove("view_details");
+    document.getElementById("METABO_details").classList.remove("view_details");
+    document.getElementById("HADEF_details").classList.remove("view_details");
+    document.getElementById("STIHL_details").classList.add("view_details");
+    document
+      .getElementById("STIHL_details")
+      .scrollIntoView({ behavior: "smooth" });
+  };
+  
+  let view_HADEF = function () {
+    document.getElementById("STABILA_details").classList.remove("view_details");
+    document.getElementById("METABO_details").classList.remove("view_details");
+    document.getElementById("STIHL_details").classList.remove("view_details");
+    document.getElementById("HADEF_details").classList.add("view_details");
+    document
+      .getElementById("HADEF_details")
+      .scrollIntoView({ behavior: "smooth" });
+  };
